35e93cc3c428d933d5e1ea605b1f2841cc48db52
Jonathan Chen (home) <chen@chinstruments.com>
2014-03-02 19:39:29 -0600

    Initial commit

---
+++ 1.1-install-log.py
@@ -0,0 +1,11 @@
+print '''
+Getting Started with Python
+- using my installation of Portable Python 2.7.3
+
+Getting Started with NLTK
+- http://www.nltk.org/install.html > https://pypi.python.org/pypi/nltk
+- python setup.py build
+- python setup.py install
+- >>> import nltk
+- >>> nltk.download()
+- Collections > book
