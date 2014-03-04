1a7660642382990b2d55f5b155c39a8b8d14ce14
Jonathan Chen (work) <ypeels@live.com>
2014-03-04 12:43:08 -0600

    Man, I'm inconsistent in directory naming

--- nltk_cmd.bat
+++ nltk_cmd.bat
@@ -9,9 +9,13 @@ rem http://www.nltk.org/data.html - instead, set ANOTHER environment variable
 set NLTK_DATA=%CD%\nltk_data
 
 rem Yapari saikyou 
-rem http://stackoverflow.com/questions/5811966/windows-batch-script-get-current-drive-name
-set DRIVE=%~d0
-doskey idle="%DRIVE%\Users\Jonathan\Documents\Qt\projs-2008\Portable-Python-2.7.3.2\IDLE-Portable.exe"
+set PYTHON_DIR_HOME=D:\Users\Jonathan\Documents\Qt\projs-2008\Portable-Python-2.7.3.2
+set PYTHON_DIR_WORK=F:\Users\Jonathan\Documents\Qt\Projects-vc2008\Python-Portable-2.7.3.2
+if exist %PYTHON_DIR_HOME% (
+	doskey idle=%PYTHON_DIR_HOME%\IDLE-Portable.exe
+) else (
+	doskey idle=%PYTHON_DIR_WORK%\IDLE-Portable.exe
+)
 
 set QT_HOME=C:\Progra~2\QtSDK1.1\Desktop\Qt\4.7.3\msvc2008\bin\qtenv2.bat
 set QT_WORK=C:\Progra~2\Qt\QtSDK1.1\Desktop\Qt\4.7.3\msvc2008\bin\qtenv2.bat
