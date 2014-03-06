b3a131d713df4af85bbb3a3af1b0fa8b58e0b04c
Jonathan Chen (work) <ypeels@live.com>
2014-03-06 11:40:51 -0600

    Example 2.5 finally makes sense to me

--- code_random_text.py
+++ code_random_text.py
@@ -1,11 +1,29 @@
 # Natural Language Toolkit: code_random_text
+import nltk
 
+# tsk tsk, no docstring in the original
 def generate_model(cfdist, word, num=15):
-    for i in range(num):
+    ''' Plays a "word association game"  starting with "word" for "num" iterations.
+        
+        Precondition: cfdist = ConditionalFreqDist( conditions=words, samples=associated_words )
+    '''
+    for i in range(num):                    # num = iterations 
         print word,
-        word = cfdist[word].max()
+        word = cfdist[word].max()           # next word is whatever word is MOST associated with the previous word
 
-text = nltk.corpus.genesis.words('english-kjv.txt')
-bigrams = nltk.bigrams(text)
-cfd = nltk.ConditionalFreqDist(bigrams) # [_bigram-condition]
+        
+if __name__ == '__main__':
+    text = nltk.corpus.genesis.words('english-kjv.txt')
+    bigrams = nltk.bigrams(text)
+    cfd = nltk.ConditionalFreqDist(bigrams) # [_bigram-condition] - in this example, word2 is associated with word1 if it FOLLOWS it (completes a bigram with it) in "text"
+    
+    print cfd['living']
+    generate_model(cfd, 'living')           # I would call this PSEUDO-random text at best...
 
+# TODO
+# - move into a class? seems like you should be able to call cfd.generate_model('living')
+# - rule out punctuation, which gives silly results (try 'son')
+
+# note "funnel"
+# ',' -> 'and' -> 'the' -> 'land' -> of' -> 'the' -> ...
+                                
