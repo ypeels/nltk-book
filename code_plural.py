7480fce3e37ccb9fd975912416bdcde3329df25c
Jonathan Chen (work) <ypeels@live.com>
2014-03-06 13:44:35 -0600

    my notes

--- code_plural.py
+++ code_plural.py
@@ -1,6 +1,14 @@
+# Example 2.6
 # Natural Language Toolkit: code_plural
 
 def plural(word):
+    '''"This function tries to work out the plural form of any English noun.
+        
+        From Example 2.6.
+        As the text mentions, this is a work in progress! 
+        >>> plural('fan')  # fen
+        >>> plural('span')  # spen    
+    '''
     if word.endswith('y'):
         return word[:-1] + 'ies'
     elif word[-1] in 'sx' or word[-2:] in ['sh', 'ch']:
