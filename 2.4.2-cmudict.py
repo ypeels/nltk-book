65ed5bd81c4f090e83407ef494126df146cdae45
Jonathan Chen (work) <ypeels@live.com>
2014-03-06 17:09:42 -0600

    gave up on a nested dictcomp version

--- 2.4.2-cmudict.py
+++ 2.4.2-cmudict.py
@@ -5,22 +5,36 @@
 
 import nltk
 
-cmudict_entries = nltk.corpus.cmudict.entries()
-p3 = [(pron[0] + '-' + pron[2], word)
-        for (word, pron) in cmudict_entries
-        if pron[0] == 'P' and len(pron) == 3
-        ]
-    
-target_dict = {}    
-for (pron_str, word) in p3:
-    if not target_dict.has_key(pron_str):
-        target_dict[pron_str] = [word]
-    else:
-        target_dict[pron_str].append(word)
+all_entries = nltk.corpus.cmudict.entries()
+p3_entries = [(word, pron) for (word, pron) in all_entries if pron[0] == 'P' and len(pron) == 3]    # 3-syllable P-words
+p3 = [(pron[0] + '-' + pron[2], word) for (word, pron) in p3_entries]                               # book's data format
+
         
-for pron_str in sorted(target_dict.keys()):
-    word_list = target_dict[pron_str]
-    if len(word_list) > 10:
-        print pron_str, " ".join(sorted(word_list))
+# iterative version
+iterative_dict = { }    
+for (pron_str, word) in p3:
+    if not iterative_dict.has_key(pron_str):
+        iterative_dict[pron_str] = [word]
+    else:
+        iterative_dict[pron_str].append(word)
+        
+# # listcomp version?? ugh, could not figure this out easily
+# p3_list = [ (pron, [ w for  ]) for (pron, word) in p3]
+# 
+# p3 = { pron[0] + '-' + pron[2] : [word for (word, pron) in p3_entries]          # this feels wrong... and it 
+#         for (word, pron) in p3_entries }
+
+def output_dict(d):        
+    for pron_str in sorted(d.keys()):
+        word_list = d[pron_str]
+        if len(word_list) > 10:
+            print pron_str, " ".join(sorted(word_list))
+     
+     
+output_dict(iterative_dict)      
     
 # there's gotta be a more pythonic way to write this...
+# oh, apparently there are dict comprehensions starting in Python 2.7!? http://stackoverflow.com/questions/1747817/
+# didn't help (still iterative): http://stackoverflow.com/questions/5378231/python-list-to-dictionary-multiple-values-per-key
+# i just can't figure out how to write a "comprehension" statement that merges multiple values into a list, for a given key
+    # meh, i thnk the iterative version is PRETTY clear...
