4ecf6ca2ef9380763989a6743f7ba923a890da53
Jonathan Chen <ypeels@live.com>
2014-03-07 14:35:14 -0600

    I guess I'm done with exercise 2.4 to my satisfaction...

--- exercises/2.4-state_union.py
+++ exercises/2.4-state_union.py
@@ -3,7 +3,8 @@ import nltk
 # doing battle with the idiosyncratic nltk api...
 # "Read in the texts of the State of the Union addresses, using the state_union corpus reader."
 su = nltk.corpus.state_union
-addresses = { file[:4]: su.words(file) for file in su.fileids()} # dictcomp!
+address_dict = { file[:4]: su.words(file) for file in su.fileids()} # dictcomp!
+address_list = [(int(file[:4]), su.words(file)) for file in su.fileids()]
 
 # "Count occurrences of men, women, and people in each document. 
 # What has happened to the usage of these words over time?"
@@ -14,25 +15,51 @@ addresses = { file[:4]: su.words(file) for file in su.fileids()} # dictcomp!
 #                for year in addresses.keys()
 #                for addr
 
+# a = addresses['1945']
+# [('1945',) + tuple([ a.count(word) for word in ['men', 'women', 'people']   ])]
 
-# ugly, unpythonic, iterative implementation
-word_counts = []
-word_ratios = []
+
+# ahh, this is much more transparent once you figure out how to express it with listcomps
+# seems like the point of these listcomps is to FOCUS your attention on the data
 search_words = ['men', 'women', 'people']
-for year in sorted(addresses.keys()):
-    addr = addresses[year]
-    count = [year]
-    ratio = []
-    for word in search_words:
-        count.append(addr.count(word))
-        ratio.append(addr.count(word) / float(len(addr)))
-    word_counts.append(tuple(count))
-    word_ratios.append(tuple(ratio))
+word_count = [ 
+    (year,) + 
+    tuple([addr.count(word) for word in search_words]) +        # WOW that's transparent, ESPECIALLY compared with the iterative/imperative version. beat that, ruby!
+    tuple([addr.count(word) / float(len(addr)) for word in search_words])
+    for year, addr in address_list
+    ]
+from pprint import pprint
+pprint(word_count)     
+
+# seems like another ratio you could compute would be men/women or men/people
+# it would also be more informative to PLOT these data, but i'm too tired
+# my main objective here is to learn python well enough for the coursera course
+# but i'll learn nltk more thoroughly if/when the time comes...
+
+
+
+def iterative_version():
+    '''ugly, unpythonic, iterative first solution to Exercise 2.4'''
     
-print "Year", " ".join(search_words), "ratios"
-for count, ratio in zip(word_counts, word_ratios):
-    print count, ratio
+    addresses = address_dict  # I used my dictcomp on my first try
+    
+    word_counts = []
+    word_ratios = []
+    search_words = ['men', 'women', 'people']
+    for year in sorted(addresses.keys()):
+        addr = addresses[year]
+        count = [year]
+        ratio = []
+        for word in search_words:
+            count.append(addr.count(word))
+            ratio.append(addr.count(word) / float(len(addr)))
+        word_counts.append(tuple(count))
+        word_ratios.append(tuple(ratio))
+        
+    print "Year", " ".join(search_words), "ratios"
+    for count, ratio in zip(word_counts, word_ratios):
+        print count, ratio
+        
     
     
-    
-    
+#iterative_version()    
