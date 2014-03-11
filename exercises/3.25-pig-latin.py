857a068169037cdb016edbdcbf44f45a737b813a
Jonathan Chen (work) <ypeels@live.com>
2014-03-11 15:33:25 -0500

    phase 3: capitalization, qu, and y (which i already did...)

--- exercises/3.25-pig-latin.py
+++ exercises/3.25-pig-latin.py
@@ -8,8 +8,9 @@ import re
 
 english_pattern = (
 
-        # leading consonants (including y, which must [?] be consonant if leading)
-        r'^(y|[bcdfghjklmnpqrstvwxz]*)'          
+        # leading consonants (including y, which must [?] be consonant if leading). oops, 'y' was part of phase 3...
+        #r'^(y|[bcdfghjklmnpqrstvwxz]*)'        # phase 1
+        r'^(y|[bcdfghjklmnprstvwxz(?:qu)]*)'    # phase 3: (?:qu) to detect leading "qu"
         
         # the rest of the word, starting with its first vowel. (assumes internal 'y' is vowel-like)
         r'([aeiouy]\w*)'      
@@ -24,8 +25,13 @@ piglatin_replacement_pattern = r'\2\1ay'
 english_re = re.compile(english_pattern)
 #piglatin_re = re.compile(piglatin_replacement_pattern) # no, you don't compile the replacement pattern
 
-def pig_latinize_word(word):
-    return english_re.sub(piglatin_replacement_pattern, word)
+def pig_latinize_word(input_word):    
+    output_word = english_re.sub(piglatin_replacement_pattern, input_word.lower())
+    if input_word.istitle():                            # phase 3: maintain capitalization    
+        output_word = output_word.title()
+    return output_word
+
+    #return english_re.sub(piglatin_replacement_pattern, word)
     
 def pig_latinize_text(text):
     words = [ word for word in nltk.word_tokenize(text) if word.isalpha() ]
@@ -37,7 +43,7 @@ def pig_latinize_text(text):
     
     
 if __name__ == "__main__":
-    sentence = "the quick brown fox jumps over the lazy dog school squeak"
+    sentence = "The quick brown fox jumps over the Lazy Dog School squeak"
     for word in sentence.split():
         print word, "->", pig_latinize_word(word)
     
