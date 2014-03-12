360d83608983e37c60601722c634c054c169b84a
Jonathan Chen (work) <ypeels@live.com>
2014-03-12 15:43:21 -0500

    fixed corrupt file and typo (izip)

--- code_epytext.py
+++ code_epytext.py
@@ -9,3 +9,20 @@ def accuracy(reference, test):
     In particular, return the fraction of indexes
     {0<i<=len(test)} such that C{test[i] == reference[i]}.
 
+    @param reference: An ordered list of reference values.
+    @type reference: C{list}
+    @param test: A list of values to compare against the corresponding
+        reference values.
+    @type test: C{list}
+    @rtype: C{float}
+    @raise ValueError: If C{reference} and C{length} do not have the
+        same length.
+    """
+
+    if len(reference) != len(test):
+        raise ValueError("Lists must have the same length.")
+    num_correct = 0
+    for x, y in zip(reference, test):
+        if x == y:
+            num_correct += 1
+    return float(num_correct) / len(reference)
