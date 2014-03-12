4144506aa38eb5adc37fe7f89d6c9f865c026ccc
Jonathan Chen (work) <ypeels@live.com>
2014-03-12 15:57:03 -0500

    added test code and my notes

--- code_epytext.py
+++ code_epytext.py
@@ -1,7 +1,17 @@
+'''
+Example 4.5 from Section 4.4: Documenting Functions
+
+As downloaded, file was corrupt and had "izip" instead of "zip" inthe for loop.
+https://github.com/nltk/nltk_book/issues/49
+
+Largely noteworthy as an example of how 'NLTK uses the "epytext" markup language to document parameters.'
+'''
+
+
 # Natural Language Toolkit: code_epytext
 
 def accuracy(reference, test):
-    """
+    '''
     Calculate the fraction of test items that equal the corresponding reference items.
 
     Given a list of reference values and a corresponding list of test values,
@@ -17,12 +27,17 @@ def accuracy(reference, test):
     @rtype: C{float}
     @raise ValueError: If C{reference} and C{length} do not have the
         same length.
-    """
+    '''                                                             # NLTK uses "epytext" markup language to document parameters
 
     if len(reference) != len(test):
-        raise ValueError("Lists must have the same length.")
+        raise ValueError("Lists must have the same length.")        # ooh, silently introducing exceptions, are we?    
     num_correct = 0
-    for x, y in zip(reference, test):
+    for x, y in zip(reference, test):                               # this was originally "izip(reference, test)"
         if x == y:
             num_correct += 1
     return float(num_correct) / len(reference)
+    
+    
+if __name__ == "__main__":
+    print __doc__
+    print accuracy(['ADJ', 'N', 'V', 'N'], ['N', 'N', 'V', 'ADJ'])  # 0.5
