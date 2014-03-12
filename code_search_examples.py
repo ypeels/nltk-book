212623847450ef19b40df1728d4e11937b288aae
Jonathan Chen (work) <ypeels@live.com>
2014-03-12 16:18:40 -0500

    Partitioned test code off and added my notes

--- code_search_examples.py
+++ code_search_examples.py
@@ -1,6 +1,15 @@
+'''
+Example 4.6 in Section 4.5.2: Accumulative Functions (a.k.a. Generators)
+
+Figure 4.6: Accumulating Output into a List
+'''
+
 # Natural Language Toolkit: code_search_examples
 
 def search1(substring, words):
+    '''iterative, "standard way to" accumulate search hits into a list
+    
+    returns aggregated results as a list'''
     result = []
     for word in words:
         if substring in word:
@@ -8,14 +17,24 @@ def search1(substring, words):
     return result
 
 def search2(substring, words):
+    '''generator that will output the same results as search1(), if both are called in a for loop
+    
+    note that unlike search1(), it's up to the caller to store all aggregate results here'''
     for word in words:
         if substring in word:
             yield word
 
-print "search1:"
-for item in search1('zz', nltk.corpus.brown.words()):
-    print item
-print "search2:"
-for item in search2('zz', nltk.corpus.brown.words()):
-    print item
+            
+if __name__ == "__main__":
+    import nltk
+
+    print search1.__doc__
+    print "search1 results (will display in one big shot - cuz it's allocating huge storage):"
+    for item in search1('zz', nltk.corpus.brown.words()):
+        print item
+        
+    print search2.__doc__
+    print "search2 results (will stream out piecewise):"
+    for item in search2('zz', nltk.corpus.brown.words()):
+        print item
 
