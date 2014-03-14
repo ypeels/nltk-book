1cf60a736ff429900f3328a7b4903e01f0a38b5a
Jonathan Chen (work) <ypeels@live.com>
2014-03-14 13:38:08 -0500

    test code and my notes

--- code_check_parens.py
+++ code_check_parens.py
@@ -1,11 +1,30 @@
 # Natural Language Toolkit: code_check_parens
 
+'''
+Example 4.2 from Chapter 4 Extras, Section 4.2.1: Stacks and Queues
+'''
+
+
 def check_parens(tokens):
+    '''Figure 4.2: Check whether parentheses are balanced
+    
+    "overkill because we could have done a direct count: phrase.count('(') == phrase.count(')')"
+    
+    Limitation: will crash if there are more closing than opening parens'''
     stack = []
     for token in tokens:
         if token == '(':     # push
             stack.append(token)
         elif token == ')':   # pop
             stack.pop()
-    return stack
+    return stack                                    # evaluates to False 
 
+if __name__ == "__main__":
+     phrase = "( the cat ) ( sat ( on ( the mat )"
+     print check_parens(phrase.split())             # ['(', '(']
+     
+     try:
+        print check_parens(")")
+     except IndexError as e:
+        print "See? the stack.pop() call can't handle it when empty...", e.message
+        #raise IndexError(e)                        # if you really wanted to get the user's attention...meh
--- code_convert_parens.py
+++ code_convert_parens.py
@@ -1,15 +1,40 @@
 # Natural Language Toolkit: code_convert_parens
 
-def convert_parens(tokens):
+'''
+Example 4.3 from Chapter 4 Extras, 4.2.1: Stacks and Queues
+'''
+
+def convert_parens(tokens, debug=False):
+    '''Convert a nested phrase into a nested list using a stack
+    
+    Limitation: same return value regardless of whether parentheses are matched...
+    '''
     stack = [[]]
     for token in tokens:
         if token == '(':     # push
             sublist = []
-            stack[-1].append(sublist)
-            stack.append(sublist)
+            stack[-1].append(sublist)                       # push NESTED []
+            stack.append(sublist)                           # push reference to SAME []
         elif token == ')':   # pop
-            stack.pop()
+            stack.pop()                                     # pop running [stuff that was pushed]. 
         else:                # update top of stack
             stack[-1].append(token)
+            
+        if debug:
+            print token, '\t', stack                        # my added instrumentation
+
+    if debug:
+        print 
+        if len(stack) > 1:                                  # i think the point of pushing 2 copies is so that you can see at a glance where the imbalance begins?
+            print "Unmatched parentheses somewhere!?"
+            print stack
+            print
+            
     return stack[0]
 
+
+if __name__ == "__main__":
+    phrase = "( the cat ) ( sat ( on ( the mat ) ) )"
+    convert_parens(phrase.split()[:-1], debug=True)         # unmatched!
+    
+    print "aa", convert_parens(phrase.split())              # [['the', 'cat'], ['sat', ['on', ['the', 'mat']]]]
